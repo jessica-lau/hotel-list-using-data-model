28b191527459414a0d7493beb8c9362ad93b1ae5
jessica-lau <jessica.lau2@gmail.com>
2020-06-07 23:32:28 -0400

    made changes to data model for filters

--- src/data.js
+++ src/data.js
@@ -1,50 +1,17 @@
 //TODO: add and export the data model for your list of filters
+/* Notice how we've changed the data model for filters
+to make managing the state of the selected filters and the
+conditional rendering of our hotels easier. */
 export var filters = [
-    {
-        id: 21,
-        name: "Free Cancellation",
-        matchCount: 307
-    },
-    {
-        id: 22,
-        name: "Breakfast Included",
-        matchCount: 175
-    },
-    {
-        id: 23,
-        name: "Bestsellers",
-        matchCount: 20
-    },
-    {
-        id: 41,
-        name: "Parking",
-        matchCount: 272
-    },
-    {
-        id: 42,
-        name: "Pets Allowed",
-        matchCount: 164
-    },
-    {
-        id: 43,
-        name: "Room Service",
-        matchCount: 170
-    },
-    {
-        id: 44,
-        name: "Fitness Center",
-        matchCount: 249
-    },
-    {
-        id: 45,
-        name: "Swimming Pool",
-        matchCount: 101
-    },
-    {
-        id: 45,
-        name: "Dining Area",
-        matchCount: 80
-    }
+    { display: "Free Cancellation", key: "hasFreeCancellation" },
+    { display: "Breakfast Included", key: "includesBreakfast" },
+    { display: "Bestsellers", key: "isBestSeller" },
+    { display: "Parking", key: "hasParking" },
+    { display: "Pets Allowed", key: "allowsPets" },
+    { display: "Room Service", key: "hasRoomService" },
+    { display: "Fitness Centre", key: "hasFitnessCenter" },
+    { display: "Swimming Pool", key: "swimmingPool" },
+    { display: "Dining Area", key: "diningArea" }
 
 ];
 
@@ -65,7 +32,9 @@ export var hotels = [
         hasParking: false,
         allowsPets: false,
         hasRoomService: false,
-        hasFitnessCenter: false
+        hasFitnessCenter: false,
+        swimmingPool: true,
+        diningArea: false
     },
     {
         id: 124,
@@ -82,7 +51,9 @@ export var hotels = [
         hasParking: true,
         allowsPets: false,
         hasRoomService: true,
-        hasFitnessCenter: true
+        hasFitnessCenter: true,
+        swimmingPool: false,
+        diningArea: false
     },
     {
         id: 125,
@@ -95,10 +66,51 @@ export var hotels = [
         roomsRemaining: 2,
         hasFreeCancellation: false,
         includesBreakfast: false,
-        isBestSeller: false,
+        isBestSeller: true,
         hasParking: true,
         allowsPets: false,
         hasRoomService: false,
-        hasFitnessCenter: true
+        hasFitnessCenter: true,
+        swimmingPool: true,
+        diningArea: true
+    },
+    {
+        id: 126,
+        name: "Dream Downtown",
+        rating: 7.7,
+        reviewCount: 1519,
+        pricePerNight: 427,
+        imageSrc:
+            "https://q-ak.bstatic.com/xdata/images/hotel/square600/43860055.webp?k=5cb29412068e05723ee6b1c204685c6d3a8fab1c625f741fdc559ee67236674c&o=",
+        roomsRemaining: 14,
+        hasFreeCancellation: true,
+        includesBreakfast: false,
+        isBestSeller: false,
+        hasParking: false,
+        allowsPets: true,
+        hasRoomService: false,
+        hasFitnessCenter: true,
+        swimmingPool: true,
+        diningArea: true
+    },
+    {
+        id: 127,
+        name: "Aliz Hotel Times Square",
+        rating: 8.5,
+        reviewCount: 1271,
+        pricePerNight: 289,
+        imageSrc:
+            "https://r-ak.bstatic.com/xdata/images/hotel/square600/165926262.webp?k=2a08ad467a809bf1a8ebd2f1e2013b6c9693575b9a4cafc9a8cf8d4ee857c398&o=",
+        roomsRemaining: 8,
+        hasFreeCancellation: true,
+        includesBreakfast: false,
+        isBestSeller: false,
+        hasParking: true,
+        allowsPets: false,
+        hasRoomService: true,
+        hasFitnessCenter: false,
+        swimmingPool: false,
+        diningArea: true
     }
 ];
+
